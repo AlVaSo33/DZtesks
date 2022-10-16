4fe2b515043d1d84225cc314280cde6214c5a254
Alexander Soloviev <asolovev@yandex.ru>
2022-10-17 01:05:34 +0300

    Addition task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+  int max = number1;
+  int min = number2;
+if (number1 > number2)
+{
+    Console.WriteLine("Большее число: " +  number1); 
+    Console.WriteLine("Меньшее число: " +  number2);
+}
+if (number2 > number1)
+{
+    Console.WriteLine("Большее число: " +  number2);
+    Console.WriteLine("Меньшее число: " +  number1);
+}
+if (number1 == number2)
+{
+  Console.WriteLine("Числа одинаковые");
+}
